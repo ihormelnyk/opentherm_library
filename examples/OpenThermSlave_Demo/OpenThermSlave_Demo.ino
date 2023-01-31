13d0b9d08388c757901fa51ee851b9a28e7e55c2
Ihor Melnyk <ihor.melnyk@eleks.com>
2023-01-31 18:33:11 +0200

    slave demo update

--- examples/OpenThermSlave_Demo/OpenThermSlave_Demo.ino
+++ examples/OpenThermSlave_Demo/OpenThermSlave_Demo.ino
@@ -17,15 +17,54 @@ void IRAM_ATTR handleInterrupt() {
 }
 
 void processRequest(unsigned long request, OpenThermResponseStatus status) {
-    //receive request
     Serial.println("T" + String(request, HEX)); //master/thermostat request
 
-    //build UNKNOWN-DATAID response
-    unsigned long response = ot.buildResponse(OpenThermMessageType::UNKNOWN_DATA_ID, ot.getDataID(request), 0);
+    unsigned long response = 0;
+    OpenThermMessageID id = ot.getDataID(request);
+    uint16_t data = ot.getUInt(request);
+    float f = ot.getFloat(request);
+    switch(id)
+    {
+      case OpenThermMessageID::Status:
+      {
+        uint8_t statusRequest = data >> 8;
+        uint8_t chEnable = statusRequest && 0x1;
+        uint8_t dhwEnable = statusRequest && 0x2;
+        data &= 0xFF00;
+        //data |= 0x01; //fault indication
+        if (chEnable) data |= 0x02; //CH active
+        if (dhwEnable) data |= 0x04; //DHW active
+        if (chEnable || dhwEnable) data |= 0x08; //flame on
+        //data |= 0x10; //cooling active
+        //data |= 0x20; //CH2 active
+        //data |= 0x40; //diagnostic/service event
+        //data |= 0x80; //electricity production on
+
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+      case OpenThermMessageID::TSet:
+      {
+        response = ot.buildResponse(OpenThermMessageType::WRITE_ACK, id, data);
+        break;
+      }
+      case OpenThermMessageID::Tboiler:
+      {
+        data = ot.temperatureToData(45);
+        response = ot.buildResponse(OpenThermMessageType::READ_ACK, id, data);
+        break;
+      }
+      default:
+      {
+        //build UNKNOWN-DATAID response
+        response = ot.buildResponse(OpenThermMessageType::UNKNOWN_DATA_ID, ot.getDataID(request), 0);   
+      }
+    }
+    Serial.println("B" + String(response, HEX)); //slave/boiler response
 
     //send response
+    delay(20); //20..400ms, usually 100ms
     ot.sendResponse(response);
-    Serial.println("B" + String(response, HEX)); //slave/boiler response
 }
 
 void setup()
